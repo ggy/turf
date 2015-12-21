b68f368dae311c27ef6333e42e2e77aeb00eea75
ggy <glagasse@edd.fr>
2015-12-21 13:26:11 +0100

    after js

--- Turf/spiders/spiders.py
+++ Turf/spiders/spiders.py
@@ -1,19 +1,24 @@
 from scrapy import Spider
 from scrapy.selector import *
-from Turf.items import CourseItem
+from scrapy import log
+from Turf.items import *
+
+import csv
 import datetime
 import locale
-numdays = 365
+
 from scrapy.contrib.spiders import Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 
-class StackSpider(Spider):
-    name = "aspiturf"
+numdays = 365*5
+
+class CoursesSpider(Spider):
+    name = "aspiturf_courses"
     allowed_domains = ["aspiturf.com"]
     locale.setlocale(locale.LC_ALL, ("fr_FR", 'UTF-8'))
-    base = datetime.date(2009, 1, 1)
-    date_list = [base + datetime.timedelta(days=x) for x in range(0, numdays)]
+    base = datetime.date(2014, 12, 31)
+    date_list = [base - datetime.timedelta(days=x) for x in range(0, numdays)]
     start_urls = ["http://aspiturf.com/index.php?datejour="\
         + str(i) + "#coursejour" for i in date_list]
 
@@ -27,15 +32,69 @@ class StackSpider(Spider):
             item = CourseItem()
             item['Date'] = sel.xpath("td[1]/text()").extract()
             item['Heure'] = sel.xpath("td[2]/text()").extract()
-            item['Course'] = sel.xpath("td[3]/text()").extract()
+            item['Course'] = sel.xpath("td[3]/a/text()").extract()
             item['Reunion'] = sel.xpath("td[4]/text()").extract()
 	    item['Reun'] = sel.xpath("td[5]/text()").extract()
             item['Num'] = sel.xpath("td[6]/text()").extract()
-            item['Type'] = sel.xpath("td76]/text()").extract()
-            item['Partant'] = sel.xpath("td[8]/").extract()
-            item['Arrive'] = sel.xpath("td[9]").extract()
+            item['Type'] = sel.xpath("td[7]/text()").extract()
+            item['Partant'] = sel.xpath("td[8]/text()").extract()
+            item['Arrive'] = sel.xpath("td[9]/text()").extract()
             item['URL'] = sel.xpath("td[3]/a/@href").extract()
             item['Titre'] = sel.xpath("td[3]/a/@title").extract()
 
-            items.append(item)
-            return items
+            yield item
+            
+class ReunionSpider(Spider):
+    name = "aspiturf_reunions"
+    allowed_domains = ["aspiturf.com"]
+
+    cr = csv.reader(open("../courses.csv","rb"))
+    start_urls = ["http://aspiturf.com/PTcoursejourdet.php?id=" + str(687312 + i) for i in range(255000)]
+        
+    def parse(self, response):
+	selector = Selector(response)
+	questions = selector.xpath("""//div[@id="grandtab"]/div[@id="tableau"]/table[@id="grdatatablesauto"]/tbody/tr""")
+
+        self.logger.info("Extract %s", str(questions.extract()))
+	item = []
+	for sel in questions:
+	    item = ReunionItem()
+
+
+	    item['Cheval'] = sel.xpath("td[1]/a/text()").extract()
+	    item['Title_Cheval'] = sel.xpath("td[1]/a[1]/@title").extract()
+    	    item['Jocket'] = sel.xpath("td[1]/a[2]/@compjoch").extract()
+	    item['Today'] = sel.xpath("td[1]/a[2]/@today").extract()
+	    item['Infojojo'] = sel.xpath("td[1]/a[2]/@infojojo").extract()
+	    item['Title_Entraineur'] = sel.xpath("td[1]/a[2]/@title").extract()
+            item['Entraineur'] = sel.xpath("td[1]/a[2]/text()").extract()
+
+            item['Jvh'] = sel.xpath("td[2]/text()").extract()
+	    item['Jv'] = sel.xpath("td[3]/text()").extract()
+            item['Jp'] = sel.xpath("td[4]/text()").extract()
+            item['Evh'] = sel.xpath("td[5]/text()").extract()
+	    item['Ev'] = sel.xpath("td[6]/text()").extract()
+	    item['Ep'] = sel.xpath("td[7]/text()").extract()
+            item['Oeil'] = sel.xpath("td[8]/b/text()").extract()
+	    item['rec'] = sel.xpath("td[9]/text()").extract()
+	    item['rapProb'] = sel.xpath("td[10]/text()").extract()
+	    item['rapDir'] = sel.xpath("td[11]/b/text()").extract()
+
+	    item['MusiquePT'] = sel.xpath("td[12]/text()").extract()
+            item['MusiqueCal'] = sel.xpath("td[13]/@title").extract()
+	    item['MusiqueJoc'] = sel.xpath("td[14]/@title").extract()
+	    item['MusiqueEntr'] = sel.xpath("td[15]/@title").extract()
+
+	    item['Corde'] = sel.xpath("td[16]/text()").extract()
+	    item['Genre'] = sel.xpath("td[17]/text()").extract()
+	    item['Age'] = sel.xpath("td[18]/text()").extract()
+	    item['Handicap'] = sel.xpath("td[19]/text()").extract()
+	    item['NbCoursesCheval'] = sel.xpath("td[20]/text()").extract()
+	    item['NbVictoireCheval'] = sel.xpath("td[21]/text()").extract()
+	    item['Place'] = sel.xpath("td[22]/text()").extract()
+	    item['Victoire'] = sel.xpath("td[23]/text()").extract()
+	    item['PI'] = sel.xpath("td[24]/text()").extract()
+ 
+	    item['URL'] = response.url
+
+            yield item
